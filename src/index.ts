945cc1b2f41b55509a8492a56404bad192d04e02
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-05-09 15:03:00 +0200

    createHeatMapModel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./lib/generateSimpleModel";
 export * from "./lib/generateTreeCheckboxModel";
+export * from "./lib/generateHeatMapTableModel";
 export * from "./lib/createSimplePersonModel";
 export * from "./lib/createTimeTableModel";
