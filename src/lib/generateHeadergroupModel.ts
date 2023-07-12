7f24f6e1dc3df49b1d7022db7694ffc36ae19364
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-07-12 18:36:09 +0200

    cellgroups

--- src/lib/generateHeadergroupModel.ts
+++ src/lib/generateHeadergroupModel.ts
@@ -1,12 +1,26 @@
 import {
   AreaModelArrayOfArrays,
-  AreaModelCellGroups,
+  AreaModelCellGroups, AreaModelObjectArrayWithColumndefs,
   CellgroupFactory,
   TableFactory,
   TableModelIf
 } from "@guiexpert/table";
 import { headerGroups } from "../demodata/header-groups";
 
+export interface FalconIf {
+  "GOLD_AB":  number;
+  "GOLD_A":  number;
+  "GOLD_B":  number;
+  "GOLD_C":  number;
+  "GOLD_D":  number;
+  "GOLD_SUM_CD":  number;
+  "HOH_AB":  number;
+  "HOH_LOC":  number;
+  "HOH_A":  number;
+  "HOH_B":  number;
+  "HOH_C":  number;
+  "HOH_D":  number;
+}
 
 // TODO hier gehts weiter
 export function createHeadergroupModel(): TableModelIf {
@@ -15,13 +29,26 @@ export function createHeadergroupModel(): TableModelIf {
   const headerAreaModel = new AreaModelCellGroups("header", headerGroups, columnDefs, defaultRowHeight);
 
   console.info("headerGroups", headerGroups);
-  console.info("columnDefs", columnDefs);
-  console.info(CellgroupFactory.buildDeepMap(headerGroups));
+  console.info("columnDefs", columnDefs.map(cd=>cd.property));
+  //console.info(CellgroupFactory.buildDeepMap(headerGroups));
 
-  const arrs = Array.from(Array(10).keys()).map((r) =>
-    Array.from(Array(columnDefs.length).keys()).map((c) => `${r}/${c}`)
-  );
-  const bodyAreaModel = new AreaModelArrayOfArrays<string>("body", arrs, defaultRowHeight, columnDefs);
+  const arrs = [
+    {
+      "GOLD_AB":  12 * Math.floor(Math.random()*50),
+      "GOLD_A":  12 * Math.floor(Math.random()*50),
+      "GOLD_B":  12 * Math.floor(Math.random()*50),
+      "GOLD_C":  12 * Math.floor(Math.random()*50),
+      "GOLD_D":  12 * Math.floor(Math.random()*50),
+      "GOLD_SUM_CD":  12 * Math.floor(Math.random()*50),
+      "HOH_AB":  12 * Math.floor(Math.random()*50),
+      "HOH_LOC":  12 * Math.floor(Math.random()*50),
+      "HOH_A":  12 * Math.floor(Math.random()*50),
+      "HOH_B":  12 * Math.floor(Math.random()*50),
+      "HOH_C":  12 * Math.floor(Math.random()*50),
+      "HOH_D":  12 * Math.floor(Math.random()*50),
+    }
+  ];
+  const bodyAreaModel = new AreaModelObjectArrayWithColumndefs<FalconIf>("body", arrs, columnDefs, 34);
 
   return TableFactory.createTableModel({
     headerAreaModel,
